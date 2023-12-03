63d888b470e293ea72d9b3929f8f877a546bb35b
CargoCultScientist <martin.sona@gmail.com>
2023-12-03 21:13:29 +0100

    Update AccurateSTFT.py

--- E-ENF(GUI)/AccurateSTFT.py
+++ E-ENF(GUI)/AccurateSTFT.py
@@ -1,23 +1,29 @@
-import numpy as np
-from scipy.fftpack import fft
+import numpy as np  # Import the NumPy library for numerical operations
+from scipy.fftpack import fft  # Import the FFT function from SciPy library
 
-def AccurateSTFT (Signal,Window,StepPoints,Fs,NFFT):
-   FrameSize       = Window
-   Signal2         = np.zeros(int(FrameSize/2)*2 + len(Signal))
-   Signal2[int(FrameSize/2):int(FrameSize/2)+len(Signal)] = Signal
-   WindowPositions = range(1, len(Signal2) - int(FrameSize)+2, StepPoints)
-   IF0             = np.zeros(len(WindowPositions))
+def AccurateSTFT(Signal, Window, StepPoints, Fs, NFFT):
+    # Initialization and Signal Padding
+    FrameSize = Window  # Set the frame size to the specified window size
+    Signal2 = np.zeros(int(FrameSize / 2) * 2 + len(Signal))  # Create an array to hold the padded signal
+    Signal2[int(FrameSize / 2):int(FrameSize / 2) + len(Signal)] = Signal # Pad the original signal with zeros to match the frame size
+    WindowPositions = range(1, len(Signal2) - int(FrameSize) + 2, StepPoints) # Calculate positions for overlapping windows
+    
+    IF0 = np.zeros(len(WindowPositions))  # Initialize an array to store instantaneous frequencies
 
+    # Loop through each window position
+    for i in range(len(WindowPositions)):
+        B = np.array(fft(Signal2[WindowPositions[i]:WindowPositions[i] + int(FrameSize)], NFFT))  # Perform FFT on the windowed signal
+        HalfTempFFT = B[:int(len(B) / 2)]  # Extract the first half of the FFT
+        absHalfTempFFT = abs(HalfTempFFT)  # Compute the absolute values of the FFT coefficients
 
-   for i in range(len(WindowPositions)):
-      B                     = np.array(fft(Signal2[WindowPositions[i]:WindowPositions[i] + int(FrameSize)], NFFT))
-      HalfTempFFT           = B[:int(len(B)/2)]
-      absHalfTempFFT        = abs(HalfTempFFT)
-      PeakLoc               = np.argmax(absHalfTempFFT)
-      ValueLeft             = HalfTempFFT[PeakLoc - 1]
-      ValueCenter           = HalfTempFFT[PeakLoc]
-      ValueRight            = HalfTempFFT[PeakLoc + 1]
-      lalalal               = (ValueRight - ValueLeft)/(2*ValueCenter - ValueRight - ValueLeft)
-      CorrectionCoef        = -lalalal.real
-      IF0[i]                = (PeakLoc + CorrectionCoef - 1) * Fs / NFFT
-   return IF0
+        # Find the peak frequency in the FFT
+        PeakLoc = np.argmax(absHalfTempFFT)  # Find the index of the maximum value
+        ValueLeft = HalfTempFFT[PeakLoc - 1]  # Value to the left of the peak
+        ValueCenter = HalfTempFFT[PeakLoc]  # Value at the peak
+        ValueRight = HalfTempFFT[PeakLoc + 1]  # Value to the right of the peak
+
+        # Calculate correction coefficient for more accurate peak frequency estimation
+        CorrectionCoef = -((ValueRight - ValueLeft) / (2 * ValueCenter - ValueRight - ValueLeft)).real # Compute the correction coefficient
+        IF0[i] = (PeakLoc + CorrectionCoef - 1) * Fs / NFFT  # Calculate and store the instantaneous frequency
+
+    return IF0  # Return the array of instantaneous frequencies
